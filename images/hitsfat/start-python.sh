dc96730f7763ccc4738b9a4b0882455ee15196a9
Volker Gaibler <volker.gaibler@h-its.org>
2017-12-14 15:50:19 +0100

    Updated start-python.sh for hitsfat

--- images/hitsfat/start-python.sh
+++ images/hitsfat/start-python.sh
@@ -5,26 +5,31 @@
 # - working directory is the script directory
 # - exec so that it receives unix signals
 
+echo "[+] start-python.sh:"
+echo "[+] cwd = $( pwd )"
+
 # python file to start
-mainpy="$1"
+script=$( readlink -e "$1" )
 
-# path to virtual environment, with default value
+
+if [ ! -r "$script" ] ; then
+    echo "[+] Error: could not read script '$script'."
+    exit 1
+fi
+
+# python virtual environment
 PYTHON_VENV=${PYTHON_VENV:-/opt/venv2}
-
-# change working directory
-wd="$( dirname $(readlink -e $0) )"
-echo "[+] cd to script directory '$wd'"
-cd "$wd"
-
 activate="$PYTHON_VENV/bin/activate"
 if [ ! -r "$activate" ] ; then
     echo "[+] Error: virtual environment '$PYTHON_VENV' not found."
     exit 1
 fi
-if [ ! -r "$mainpy" ] ; then
-    echo "[+] Error: script '$mainpy' not readable."
-fi
-
 echo "[+] activating virtual environment '$PYTHON_VENV'"
 . "$activate"
-exec python $mainpy
+
+# change working directory
+wd=$( dirname "$script" )
+echo "[+] cd to script directory '$wd'"
+cd "$wd"
+
+exec python $script
